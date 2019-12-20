31b1ae0d95211ed6d36695704fe750d80ffc8fdc
Adam Rafiandri <rafiandria23@gmail.com>
2019-12-20 19:12:43 +0700

    Added search bar and an article about me

--- doms/body.js
+++ doms/body.js
@@ -33,6 +33,98 @@ aboutMe.setAttribute('href', '#');
 aboutMe.innerHTML = 'About Me';
 topMenuLinkDiv.appendChild(aboutMe);
 
+
+
+// MAIN SECTION
+
+let main = document.createElement('main');
+
+// SEARCH BAR
+
+let searchBar = document.createElement('input');
+searchBar.setAttribute('class', 'search-bar');
+searchBar.setAttribute('id', 'main-search-bar');
+searchBar.setAttribute('type', 'searchBar');
+searchBar.setAttribute('onkeyup', 'searchFunc()');
+searchBar.setAttribute('placeholder', 'What do you want to learn?');
+searchBar.setAttribute('title', 'Type anything');
+
+main.appendChild(searchBar);
+
+const searchFunc = function() {
+    // Declare variables
+    let input = document.getElementsByClassName('search-bar');
+    let filter = input.value.toLowerCase();
+    let articleDiv = document.getElementsByClassName('article-div');
+    let searchItem = articleDiv.getElementsByClassName('article');
+
+    // Loop through all titles and hide those that don't match the search query
+    for (let i = 0; i < searchItem.length; i++) {
+        let a = searchItem[i].getElementsByClassName('article-title');
+        if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
+            searchItem[i].style.display = '';
+        }
+        else {
+            searchItem[i].style.display = 'none';
+        }
+    }
+}
+
+
+
+
+// ARTICLE SECTION
+
+const createArticleDiv = function (className, title, content) {
+    if (className === undefined) {
+        alert('Please provide the article class name.');
+    }
+    else if (title === undefined) {
+        alert('Please provide the article title.');
+    }
+    else if (className && title === undefined) {
+        alert('Please provide the article class name and title.');
+    }
+    
+    let articleDiv = document.createElement('div');
+    articleDiv.setAttribute('class', className);
+    main.appendChild(articleDiv);
+    
+    let articleTitle = document.createElement('a');
+    articleDiv.setAttribute('class', 'article-title');
+    articleTitle.innerHTML = title;
+    articleDiv.appendChild(articleTitle);
+    
+    if (content === undefined) {
+        return articleDiv;
+    }
+    else {
+        articleDiv.appendChild(content);
+    }
+    
+
+    return articleDiv;
+}
+
+
+let aboutMeContent = document.createElement('p');
+aboutMeContent.setAttribute('class', 'article-content');
+aboutMeContent.innerHTML = 'Adam Rafiandri is an Aviation Maintenance Electrician graduated from SMK Penerbangan Agkasa Bogor located near the Atang Sendjaja Air Force Base. His passion in Aviation and Programming have brought him to the amazing world of both and also made him always feel fascinated about them.'
+
+let articleAboutMeDiv = createArticleDiv('article-div', 'About Me', aboutMeContent);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // FOOTER
 let footer = document.createElement('footer');
 footer.setAttribute('class', 'col-1');
@@ -50,7 +142,7 @@ footer.appendChild(footerMenuLinkDiv);
 
 let aboutMeFooter = document.createElement('a');
 aboutMeFooter.setAttribute('class', 'footer-menu-link');
-aboutMeFooter.setAttribute('href', '#');
+aboutMeFooter.setAttribute('href', './about-me.html');
 aboutMeFooter.innerHTML = 'About Me';
 footerMenuLinkDiv.appendChild(aboutMeFooter);
 
@@ -62,6 +154,7 @@ footer.appendChild(socialIconsDiv);
 //GITHUB SOCIAL ICON
 let githubAnchor = document.createElement('a');
 githubAnchor.setAttribute('class', 'footer-icon');
+githubAnchor.setAttribute('href', 'https://github.com/rafiandria23');
 
 let githubIcon = document.createElement('i');
 githubIcon.setAttribute('class', 'fab fa-github fa-2x');
@@ -71,4 +164,5 @@ socialIconsDiv.appendChild(githubAnchor);
 
 // APPENDIX
 document.body.appendChild(header);
+document.body.appendChild(main);
 document.body.appendChild(footer);
